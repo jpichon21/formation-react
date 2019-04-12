7bcc176f49a837520f5e7996db293d1826bf71e3
jpichon21 <juliencfpichon@gmail.com>
2019-04-12 10:02:25 +0200

    config

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+class App extends Component {
+  render () {
+    return <h1>Hello World !</h1>
+  }
+}
+
+export default hot(module)(App)
